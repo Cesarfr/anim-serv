e4bf67354378f9a25645c745f52c04f477299799
Cesar Hernandez Antonio <hernandez.antonio.cesar19@gmail.com>
2015-06-12 11:18:34 +0200

    Refactorizando codigo, separacion de los callbacks a funciones

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ const http = require('http')
 // Lectura del puerto a utilizar desde la variable de entorno, sino utiliza el puerto por default 8080
 const port = process.env.PORT || 8080
 // Server creado, agregado callback
-const server = http.createServer(function (req, res){
-    res.end("Hola io.js")
-})
-
+const server = http.createServer(onRequest)
 // Puerto que utilizara el servidor
-server.listen(port, function(){
+server.listen(port, onListening)
+
+// Refactorizando codigo
+function onRequest(req, res){
+    res.end("Hola io.js")
+}
+function onListening(){
     console.log("Servidor escuchando en el puerto: "+port)
-})
+}
