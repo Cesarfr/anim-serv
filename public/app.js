768cd292cbfef83bccf58ae3c9f6992d840d4a1e
Cesar Hernandez Antonio <hernandez.antonio.cesar19@gmail.com>
2015-06-12 13:01:00 +0200

    Reparado, Ahora se pueden cargar index.html y app.js

--- public/app.js
+++ public/app.js
@@ -1,1 +1,6 @@
-console.log("Hello world");
+const record = document.querySelector('#record')
+
+record.addEventListener('click', function (e) {
+    e.preventDefault()
+    console.log('Boton presionado')
+})
--- server.js
+++ server.js
@@ -23,18 +23,51 @@ server.listen(port)
 
 // Refactorizando codigo
 function onRequest(req, res) {
+    // verificar la url
+    let uri = req.url
+    if(uri.startsWith('/index') || uri === '/index.html') {
+        return serverIndex(res)
+    }
+    if(uri === '/app.js'){
+        return serverApp(res)
+    }
+    res.statusCode = 404
+    res.end(`404 not found: ${uri}`)
+}
+
+// Funcion que envia el index.html
+function serverIndex(res){
     // path join permite concatenar rutas y directorios
-    let fileName = path.join(__dirname, 'public', 'index.html')
+    let index = path.join(__dirname, 'public', 'index.html')
     
     // Especifica el tipo de cabecera a utilizar
     res.setHeader('Content-Type', 'text/html')
     
     // creacion de Read Stream
-    let rs = fs.createReadStream(fileName)
+    let rs = fs.createReadStream(index)
     rs.pipe(res)
     
     // Controlar errores
     rs.on('error', function (err) {
+        res.setHeader('Content-Type', 'text/plain')
+        res.end(err, message)
+    })
+}
+// Funcion que envia app.js
+function serverApp(res) {
+    // path join permite concatenar rutas y directorios
+    let app = path.join(__dirname, 'public', 'app.js')
+    
+    // Especifica el tipo de cabecera a utilizar
+    res.setHeader('Content-Type', 'text/javascript')
+    
+    // creacion de Read Stream
+    let rs = fs.createReadStream(app)
+    rs.pipe(res)
+    
+    // Controlar errores
+    rs.on('error', function (err) {
+        res.setHeader('Content-Type', 'text/plain')
         res.end(err, message)
     })
 }
